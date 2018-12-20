a476efa7b4328feb84530a9e9a48174e758c08be
Austin Joo <joojinhyung@gmail.com>
2018-12-20 09:33:52 -0500

    Modularized it for you moe

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
 let express = require('express');
 let bodyParser = require('body-parser');
 let mongoCompos = require('./db.js');
-let dbComponents = require('./db.js')
+let retriever = require('./retrieverController.js')
 
 let server = express();
 server.use(bodyParser.json());
 server.use(express.static(__dirname, './client.html'));
 
-app.get('/streetBreezy', (req,res) => {
-    let {id} = req.query;
-    DescriptionBox.find({id: id}).exec((err, data) => {
-        if(err){
-            res.send(err);
-        } else if(data){
-            res.send(data);
-        }
-    })
-})
+app.get('/streetBreezy', retriever);
 
 let port = 2500
 server.listen(port, () => {
