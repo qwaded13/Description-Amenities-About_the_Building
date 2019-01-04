7377181da23d9d47fcbc34bf2a26de416f4d567e
Austin Joo <joojinhyung@gmail.com>
2019-01-04 10:43:12 -0500

    Made small changes to server

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ let cors = require('cors');
 
 let server = express();
 server.use(bodyParser.json());
-server.use(express.static(path.join(__dirname, '/client/dist')));
+server.use('/:id', express.static(path.join(__dirname, '/client/dist')));
 server.use(cors());
 
-server.get('/streetBreezy/api/', retrieveFunctions.retriever);
+server.get('/streetBreezy/api/:id', retrieveFunctions.retriever);
 // server.get('/*', retrieveFunctions.alt);
 
 let port = 3009
