069975f01503dc3d36f9970f18807d86604349d2
Austin Joo <joojinhyung@gmail.com>
2019-01-04 19:18:18 -0500

    Changed href again

--- client/table.jsx
+++ client/table.jsx
@@ -18,7 +18,7 @@ var Table = () => {
                 <td className='tabletext' id='table2x0'>
                     <span>ESTIMATED PAYMENT</span>
                     <br></br>
-                    <span><a target='_blank' href='http://static.tvtropes.org/pmwiki/pub/images/nvtelen.JPG'>${parseInt((Math.random())*100000)}</a></span>
+                    <span><a target='_blank' href='https://data.whicdn.com/images/315929782/large.jpg'>${parseInt((Math.random())*100000)}</a></span>
                 </td>
             </tr>
             <tr id='tableRow1'>
