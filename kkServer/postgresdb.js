d89a4385ea5bceaa00130300f3c3cf613202e392
Kevin Kim <kevinerickim@gmail.com>
2019-01-15 15:29:08 -0500

    Create one postgres table and related schema

--- kkServer/postgresdb.js
+++ kkServer/postgresdb.js
@@ -5,11 +5,47 @@ const pool = new Pool({
   connectionString: connectionString
 })
 
-pool.query('SELECT version()')
-  .then((res) => {
-    console.log('Query result: ', res)
-    pool.end()
-  })
-  .catch((err) => {
-    console.log(err)
-  });
+// pool.query('SELECT version()')
+//   .then((res) => {
+//     console.log('Query result: ', res)
+//     pool.end()
+//   })
+//   .catch((err) => {
+//     console.log(err)
+//   });
+
+// pool.query('CREATE SCHEMA descriptions;')
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+pool.query(`
+  CREATE TABLE descriptions.descriptions (
+    id INTEGER NOT NULL PRIMARY KEY,
+    description TEXT NOT NULL,
+    highlightAmens TEXT [] NOT NULL,
+    buildingAmens TEXT [] NOT NULL,
+    listingAmens TEXT [] NOT NULL,
+    outdoorAmens TEXT [] NOT NULL
+  );
+`)
+.then((res) => {
+  console.log('table created!');
+  pool.query(`
+    CREATE INDEX id ON descriptions.descriptions (id);
+  `)
+    .then((res) => {
+      console.log('Index created on col id');
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+})
+.catch((err) => {
+  console.log(err);
+});
+
+module.exports = pool;
