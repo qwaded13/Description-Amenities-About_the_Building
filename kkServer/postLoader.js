76af66c18e75367cb264203b1ce10b8dcdabc1ac
Kevin Kim <kevinerickim@gmail.com>
2019-01-17 11:57:29 -0500

    Comment how to optimize Postgres loading func

--- kkServer/postLoader.js
+++ kkServer/postLoader.js
@@ -7,7 +7,7 @@ let createCsvWriter = require('csv-writer').createObjectCsvWriter;
 let dataTotal = 10000000;
 let batchNumber = 0;
 
-
+/* !!! make the data types for the arrays JSONB data types instead of postgres arrays */
 console.time('PG Load Time:')
 let postLoader = (currentTotal) => {
   let data = generator(500000, currentTotal, true)
@@ -37,20 +37,6 @@ let postLoader = (currentTotal) => {
     .catch((err) => {
       console.log(err);
     })
-
-  // DescriptionBox.collection.insertMany(data, (err, doc) => {
-  //   if (err) console.log(err);
-  //   else if (currentTotal < dataTotal) {
-  //     if (!(currentTotal % 1000000)){
-  //       batchNumber++;
-  //       console.log(`Loading batch #${batchNumber}/10`);
-  //     } 
-  //     currentTotal += 100000
-  //     loader();
-  //   } else {
-  //     if (currentTotal >= dataTotal) console.timeEnd('loadTime')
-  //   }
-  // })
 }
 
 postLoader(0);
