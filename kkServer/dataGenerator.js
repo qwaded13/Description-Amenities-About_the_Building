7befc750188833028f17f142fc3ed49f024dca99
Kevin Kim <kevinerickim@gmail.com>
2019-01-17 11:58:29 -0500

    Increase description size for read-more functionality

--- kkServer/dataGenerator.js
+++ kkServer/dataGenerator.js
@@ -1,20 +1,20 @@
 //Dependencies
 let faker = require('faker');
-let fs = require('fs');
+// let fs = require('fs');
 
 //Other modules
-// let db = require('../db.js');
+let db = require('../db.js');
 
 let highlightAmenities = ['Cats and Dogs Allowed', 'Full-time Doorman', 'Washer/Dryer In-Unit', 'Elevator']
 let buildingAmenities = ['Bike Room', 'Green Building', 'Package Room', 'Children\'s playroom', 'Concierge', 'Laundry in Building', 'Smoke-free', 'Storage Available', 'Live-in Super', 'Cold-Storage', 'Gym', 'Virtual Doorman', 'Concierge', 'Media Room', 'Community Recreation Facility', 'Swimming Pool', 'Parking Available']
 let listingAmenities = ['Central Air-Conditioning', 'Dishwasher', 'Storage Available', 'Fireplace']
 let outdoorAmenities = ['Terrace', 'Roof Deck', 'Garden']
-let description = faker.lorem.words(num = 500);
+let description = faker.lorem.words(num = 200);
 
 let generator = function(batchSize, startID, csvFormat) {
   let records = [];
   for (let i = startID; i < startID + batchSize; i++) {
-    let descriptionStart = Math.floor((Math.random() * 300));
+    let descriptionStart = Math.floor((Math.random() * 500));
     let highlightStart = Math.floor((Math.random() * highlightAmenities.length));
     let buildingStart = Math.floor((Math.random() * buildingAmenities.length));
     let listingStart = Math.floor((Math.random() * listingAmenities.length));
@@ -25,7 +25,7 @@ let generator = function(batchSize, startID, csvFormat) {
       let regex2 = new RegExp('\\]');
       records.push({
         id: i,
-        description: description.slice(descriptionStart, descriptionStart + 190),
+        description: description.slice(descriptionStart, descriptionStart + 300),
         highlightAmens: JSON.stringify(highlightAmenities.slice(highlightStart, highlightStart + Math.floor(Math.random() * 4))).replace(regex1, '{').replace(regex2, '}'),
         buildingAmens: JSON.stringify(buildingAmenities.slice(buildingStart, buildingStart + Math.floor(Math.random() * 4))).replace(regex1, '{').replace(regex2, '}'),
         listingAmens: JSON.stringify(listingAmenities.slice(listingStart, listingStart + Math.floor(Math.random() * 4))).replace(regex1, '{').replace(regex2, '}'),
@@ -34,7 +34,7 @@ let generator = function(batchSize, startID, csvFormat) {
     } else {
       records.push({
         id: i,
-        description: description.slice(descriptionStart, descriptionStart + 190),
+        description: description.slice(descriptionStart, descriptionStart + 300),
         highlightAmens: highlightAmenities.slice(highlightStart, highlightStart + Math.floor(Math.random() * 4)),
         buildingAmens: buildingAmenities.slice(buildingStart, buildingStart + Math.floor(Math.random() * 4)),
         listingAmens: listingAmenities.slice(listingStart, listingStart + Math.floor(Math.random() * 4)),
