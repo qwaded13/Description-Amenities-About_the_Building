5481365d891fc8b6f086c4d705695cfab35a0b4e
Kevin Kim <kevinerickim@gmail.com>
2019-01-17 12:34:28 -0500

    Create ordered index in separate step in DB

--- db.js
+++ db.js
@@ -10,8 +10,7 @@ var db = mongoose.connect('mongodb://localhost:27017/streetBreezy', (err => {
 let DescriptionBoxSchema = mongoose.Schema({
     id: {
       type: Number,
-      unique: true,
-      index: true
+      unique: true
     },
     description: String,
     highlightAmens: Array,
@@ -19,48 +18,49 @@ let DescriptionBoxSchema = mongoose.Schema({
     listingAmens: Array,
     outdoorAmens: Array
 })
+DescriptionBoxSchema.index({id: 1});
 
 let DescriptionBox = mongoose.model('descriptionBox', DescriptionBoxSchema);
 
-let saver = (description, highlightAmenitiesCreator, highlightAmenitiesArray, buildingAmenitiesCreator, buildingAmenitiesArray, listingAmenitiesCreator, listingAmenitiesArray, outdoorAmenitiesCreator, outdoorAmenitiesArray) => {
-    console.log('SAVER CALLED');
-    let divider = 1;
-    for(let i = 0; i < 100; i++){
-        if(i >= 0 && i < 25){
-            divider = 4;
-        } else if(i >= 25 && i < 50){
-            divider = 3;
-        } else if(i >= 50 && i < 75){
-            divider = 2;
-        } else if(i >= 75 && i < 100){
-            divider = 1;
-        }
-        let input1 = highlightAmenitiesCreator(divider, highlightAmenitiesArray);
-        //console.log('INPUT1: ', input1)
-        let input2 = buildingAmenitiesCreator(divider, buildingAmenitiesArray);
-        //console.log('INPUT2: ', input2)
-        let input3 = listingAmenitiesCreator(divider, listingAmenitiesArray);
-        //console.log('INPUT3: ', input3)
-        let input4 = outdoorAmenitiesCreator(divider, outdoorAmenitiesArray);
-        //console.log('INPUT4: ', input4)
-        DescriptionBox.findOne({id: i}, (err, exists) => {
-            if(err){
-                console.log('THERE IS AN ERROR!!!' , err);
-            } else if (!exists){
-                let newDescriptionBox = new DescriptionBox({
-                    id: i,
-                    description: description, 
-                    highlightAmens: input1 || null, 
-                    buildingAmens: input2 || null,
-                    listingAmens: input3 || null, 
-                    outdoorAmens: input4 || null 
-                }); 
-                newDescriptionBox.save();
-            }
-        })
-    }
-}
+// let saver = (description, highlightAmenitiesCreator, highlightAmenitiesArray, buildingAmenitiesCreator, buildingAmenitiesArray, listingAmenitiesCreator, listingAmenitiesArray, outdoorAmenitiesCreator, outdoorAmenitiesArray) => {
+//     console.log('SAVER CALLED');
+//     let divider = 1;
+//     for(let i = 0; i < 100; i++){
+//         if(i >= 0 && i < 25){
+//             divider = 4;
+//         } else if(i >= 25 && i < 50){
+//             divider = 3;
+//         } else if(i >= 50 && i < 75){
+//             divider = 2;
+//         } else if(i >= 75 && i < 100){
+//             divider = 1;
+//         }
+//         let input1 = highlightAmenitiesCreator(divider, highlightAmenitiesArray);
+//         //console.log('INPUT1: ', input1)
+//         let input2 = buildingAmenitiesCreator(divider, buildingAmenitiesArray);
+//         //console.log('INPUT2: ', input2)
+//         let input3 = listingAmenitiesCreator(divider, listingAmenitiesArray);
+//         //console.log('INPUT3: ', input3)
+//         let input4 = outdoorAmenitiesCreator(divider, outdoorAmenitiesArray);
+//         //console.log('INPUT4: ', input4)
+//         DescriptionBox.findOne({id: i}, (err, exists) => {
+//             if(err){
+//                 console.log('THERE IS AN ERROR!!!' , err);
+//             } else if (!exists){
+//                 let newDescriptionBox = new DescriptionBox({
+//                     id: i,
+//                     description: description, 
+//                     highlightAmens: input1 || null, 
+//                     buildingAmens: input2 || null,
+//                     listingAmens: input3 || null, 
+//                     outdoorAmens: input4 || null 
+//                 }); 
+//                 newDescriptionBox.save();
+//             }
+//         })
+//     }
+// }
 
 module.exports.db = db;
-module.exports.saver = saver;
+// module.exports.saver = saver;
 module.exports.DescriptionBox = DescriptionBox;
